19f81287800ce9d0c95a9e2c5358b92c35af5cdd
dac <60971@ipiaget.pt>
2025-09-02 00:27:39 +0100

    fix login...

--- Stockly-Client/app/login.tsx
+++ Stockly-Client/app/login.tsx
@@ -32,7 +32,7 @@ const login = () => {
         return (
         <>
          <View style={styles.container}>
-            <View style={[styles.loginForm, Platform.OS !== 'web' ? {height:'60%'} : {height:'50%'}]}>
+            <View style={[styles.loginForm, Platform.OS !== 'web' ? {height:'60%'} : {height:'40%'},Platform.OS == 'web' ? {width:400} : {width:300}]}>
                 <Text style={Style.StocklyTitle}>Stockly</Text>
                 <View style={[styles.textInputViews,{marginTop: 50}]}>
                     <Controller control={control} rules={{required: true}} 
